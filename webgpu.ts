01f37cd0e973b7561fa98362387b0db931af54cc
Atul Varma <varmaa@gmail.com>
2023-07-29 15:06:36 -0400

    ONLY render depth texture.

--- webgpu.ts
+++ webgpu.ts
@@ -17,12 +17,6 @@ if (!adapter) {
 
 const device = await adapter.requestDevice();
 
-const context = canvas.getContext("webgpu")!;
-
-const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-
-context.configure({device, format: canvasFormat});
-
 async function fetchShader(device: GPUDevice, filename: string): Promise<GPUShaderModule> {
     const response = await fetch(filename);
     if (!response.ok) {
@@ -40,7 +34,7 @@ const shaders = await fetchShader(device, "shaders.wgsl");
 const vertices = new Float32Array([
     0.0, 0.1,
     0.75, 0.9,
-    -1.0, 0.5,
+    -0.8, 0.5,
     1.0, 0.5,
 ]);
 
@@ -93,13 +87,6 @@ const pipeline = device.createRenderPipeline({
         entryPoint: "vertexMain",
         buffers: [vertexBufferLayout]
     },
-    fragment: {
-        module: shaders,
-        entryPoint: "fragmentMain",
-        targets: [{
-            format: canvasFormat
-        }],
-    }
 });
 
 const stagingBufferSize = WIDTH * HEIGHT * 4;
@@ -112,14 +99,7 @@ function draw() {
     const encoder = device.createCommandEncoder();
 
     const pass = encoder.beginRenderPass({
-        colorAttachments: [{
-            view: context.getCurrentTexture().createView(),
-            loadOp: "clear",
-            clearValue: {
-                r: 0, g: 0, b: 0, a: 1
-            },
-            storeOp: "store"
-        }],
+        colorAttachments: [],
         depthStencilAttachment: {
             view: depthView,
             depthLoadOp: "clear",
